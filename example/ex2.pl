d6a3efb6367e632cf7f168c1f422d1d3b8be69d1
ehonlia <hongxin.liang@ericsson.com>
2015-04-07 12:01:04 +0200

    some frequently used utilities

---
+++ example/ex2.pl
@@ -0,0 +1,38 @@
+:- module(ex2, []).
+
+:- use_module(library(ldap4pl)).
+:- use_module(library(ldap4pl_util)).
+
+:- debug(ex2).
+
+search :-
+    ldap_initialize(LDAP, 'ldap://172.16.0.223:389'),
+    debug(ex2, 'LDAP ~w', [LDAP]),
+    ldap_set_option(LDAP, ldap_opt_protocol_version, 3),
+    ldap_simple_bind_s(LDAP, 'cn=admin,dc=cf,dc=ericsson,dc=net', s3cret),
+    ldap_search(LDAP,
+        query(
+            base('dc=cf,dc=ericsson,dc=net'),
+            scope(ldap_scope_onelevel),
+            filter('(objectClass=*)'),
+%            attrs([objectClass, sambaDomainName]),
+            attrsonly(false)
+        ),
+        MsgID),
+    ldap_result(LDAP, MsgID, true, Result),
+    debug(ex2, 'Result ~w', [Result]),
+    ldap_parse_result(LDAP, Result, ErrorCode, MatchedDN, ErrorMsg, Referrals, SCtrls, false),
+    debug(ex2, 'ErrorCode ~w', [ErrorCode]),
+    debug(ex2, 'MatchedDN ~w', [MatchedDN]),
+    debug(ex2, 'ErrorMsg ~w', [ErrorMsg]),
+    debug(ex2, 'Referrals ~w', [Referrals]),
+    debug(ex2, 'SCtrls ~w', [SCtrls]),
+    ldap_parse_search_result(LDAP, Result, List),
+    print_term(List, []),
+    ldap_msgfree(Result),
+    ldap_unbind(LDAP).
+
+auth :-
+    ldap_auth('ldap://172.16.0.223:389',
+        'cn=admin,dc=cf,dc=ericsson,dc=net',
+        s3cret).
---
+++ prolog/ldap4pl_util.pl
@@ -0,0 +1,54 @@
+:- module(ldap4pl_util, [
+    ldap_parse_search_result/3,
+    ldap_auth/3
+]).
+
+:- use_module(library(ldap4pl)).
+
+ldap_parse_search_result(LDAP, Result, List) :-
+    iterate_entries(LDAP, Result, List).
+
+iterate_entries(LDAP, Result, List) :-
+    (   ldap_first_entry(LDAP, Result, Entry)
+    ->  iterate_entries0(LDAP, Entry, List0),
+        parse_entry(LDAP, Entry, Dict),
+        List = [Dict|List0]
+    ;   List = []
+    ).
+
+iterate_entries0(LDAP, Entry, List) :-
+    (   ldap_next_entry(LDAP, Entry, NextEntry)
+    ->  iterate_entries0(LDAP, NextEntry, List0),
+        parse_entry(LDAP, NextEntry, Dict),
+        List = [Dict|List0]
+    ;   List = []
+    ).
+
+parse_entry(LDAP, Entry, Dict) :-
+    iterate_attributes(LDAP, Entry, Attributes),
+    ldap_get_dn(LDAP, Entry, DN),
+    Dict = _{dn:DN, attributes:Attributes}.
+
+iterate_attributes(LDAP, Entry, Attributes) :-
+    (   ldap_first_attribute(LDAP, Entry, Attribute, Ber)
+    ->  iterate_attributes0(LDAP, Entry, Ber, Attributes0),
+        ldap_get_values(LDAP, Entry, Attribute, Values),
+        Attributes = Attributes0.put(Attribute, Values)
+    ;   Attributes = _{}
+    ).
+
+iterate_attributes0(LDAP, Entry, Ber, Attributes) :-
+    (   ldap_next_attribute(LDAP, Entry, Attribute, Ber)
+    ->  iterate_attributes0(LDAP, Entry, Ber, Attributes0),
+        ldap_get_values(LDAP, Entry, Attribute, Values),
+        Attributes = Attributes0.put(Attribute, Values)
+    ;   ldap_ber_free(Ber, false),
+        Attributes = _{}
+    ).
+
+ldap_auth(URI, Who, Passwd) :-
+    setup_call_cleanup(
+        (ldap_initialize(LDAP, URI), ldap_set_option(LDAP, ldap_opt_protocol_version, 3)),
+        ldap_simple_bind_s(LDAP, Who, Passwd),
+        ldap_unbind(LDAP)
+    ).
