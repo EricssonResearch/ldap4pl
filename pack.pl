e403e231f34a2b2a1d1fd23c9dd03a9a2ff7f276
ehonlia <hongxin.liang@ericsson.com>
2015-04-09 13:28:51 +0200

    update home

--- pack.pl
+++ pack.pl
@@ -1,9 +1,9 @@
 name(ldap4pl).
 version('0.1').
 title('LDAP interface').
-keywords([ldap]).
+keywords([ldap, binding]).
 author('Hongxin Liang', 'hongxin.liang@ericsson.com').
 packager('Hongxin Liang', 'hongxin.liang@ericsson.com').
 maintainer('Hongxin Liang', 'hongxin.liang@ericsson.com').
-home('').
-download('').
+home('http://git.cf.ericsson.net/ehonlia/ldap4pl').
+download('http://git.cf.ericsson.net/ehonlia/ldap4pl/repository/archive.tar.gz').
