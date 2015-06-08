a9c2c16e4b77d22280a3815f975b3bc510419710
ehonlia <hongxin.liang@ericsson.com>
2015-06-08 08:53:50 +0200

    0.1

--- pack.pl
+++ pack.pl
@@ -6,4 +6,4 @@ author('Hongxin Liang', 'hongxin.liang@ericsson.com').
 packager('Hongxin Liang', 'hongxin.liang@ericsson.com').
 maintainer('Hongxin Liang', 'hongxin.liang@ericsson.com').
 home('https://github.com/EricssonResearch/ldap4pl').
-download('https://github.com/EricssonResearch/ldap4pl/archive/master.zip').
+download('https://github.com/EricssonResearch/ldap4pl/archive/v0.1.zip').
